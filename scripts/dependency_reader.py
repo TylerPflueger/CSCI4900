1552cf949ec42f8858bc8c26577e23d4229a621e
Tyler Pflueger <TylerPflueger@hotmail.com>
2016-03-13 23:17:13 -0500

    Refactor code

---
+++ scripts/dependency_reader.py
@@ -0,0 +1,60 @@
+from treelib import Node, Tree
+from dependency_node import DependencyNode
+import subprocess
+import re
+from tempfile import mkdtemp
+import os
+import shutil
+
+class DependencyReader:
+    """DependencyReader object"""
+
+    def __init__(self):
+        self.tempDirectoryPath = mkdtemp(dir=".")
+        self.tree = Tree()
+        self.dependencies = {}
+        self.graphRelationships = []
+
+    def getPom(self, pomPath):
+        shutil.copy(pomPath, self.tempDirectoryPath)
+        os.chdir(self.tempDirectoryPath)
+
+    def getDependencies(self):
+        mavenTreeOutput = subprocess.Popen('mvn dependency:tree -DoutputType=tgf', stdout=subprocess.PIPE, shell=True)
+        os.chdir(os.pardir)
+
+        while True:
+            line = mavenTreeOutput.stdout.readline().rstrip()
+
+            if not line or re.search(r"BUILD SUCCESS", line):
+                break
+
+            match = re.match(r"\[INFO\]\s(\d*)\s*(.*):(.*):(\w+):([0-9\.]*)", line)
+
+            if match:
+                if not match.group(1) in self.dependencies.keys():
+                    self.dependencies[match.group(1)] = DependencyNode(match.group(2), match.group(3), match.group(5), match.group(1))
+
+                if not self.tree.leaves():
+                    self.tree.create_node(match.group(1), match.group(1), data=self.dependencies[match.group(1)])
+
+                self.dependencies[match.group(1)].get('jar', self.tempDirectoryPath)
+
+            match = re.match(r"\[INFO\]\s(\d*)\s(\d*)", line)
+
+            if match and match.group(2):
+                self.graphRelationships.append((match.group(1), match.group(2)))
+
+    def relateDependencies(self):
+        while self.graphRelationships:
+            for item in self.graphRelationships:
+                node = self.tree.get_node(item[0])
+
+                if node is not None:
+                    parent = self.dependencies[item[0]]
+                    child = self.dependencies[item[1]]
+                    self.tree.create_node(child.referenceId, child.referenceId, parent=parent.referenceId, data=child)
+                    self.graphRelationships.remove(item)
+
+    def scanDependencies(self):
+        subprocess.call('dosocs2 scan ' + self.tempDirectoryPath, shell=True)
--- scripts/main.py
+++ scripts/main.py
@@ -1,53 +1,18 @@
-from treelib import Node, Tree
-import subprocess
-import re
-from dependency_node import DependencyNode
-from tempfile import mkdtemp
 import shutil
+import sys
+import os
+from dependency_reader import DependencyReader
 
-tempDirectoryPath = mkdtemp(dir=".")
+def main():
+    dependencyReader = DependencyReader()
 
-value = subprocess.Popen('mvn dependency:tree -DoutputType=tgf', stdout=subprocess.PIPE, shell=True)
+    dependencyReader.getPom(os.path.abspath(sys.argv[1]))
+    dependencyReader.getDependencies()
+    dependencyReader.relateDependencies()
+    dependencyReader.scanDependencies()
 
-tree = Tree()
-dependencies = {}
-val = []
+    dependencyReader.tree.show()
+    shutil.rmtree(dependencyReader.tempDirectoryPath)
 
-def getDependencies():
-    while True:
-        line = value.stdout.readline().rstrip()
-
-        if not line or re.search(r"BUILD SUCCESS", line):
-            break
-
-        # This isn't working for output with version:compile(or whatever else)
-        match = re.match(r"\[INFO\]\s(\d*)\s*(.*):(.*):(\w+):([0-9\.]*)", line)
-
-        if match:
-            if not match.group(1) in dependencies.keys():
-                dependencies[match.group(1)] = DependencyNode(match.group(2), match.group(3), match.group(5), match.group(1))
-
-            if not tree.leaves():
-                tree.create_node(match.group(1), match.group(1), data=dependencies[match.group(1)])
-
-            dependencies[match.group(1)].get('jar', tempDirectoryPath)
-
-        match = re.match(r"\[INFO\]\s(\d*)\s(\d*)", line)
-
-        if match and match.group(2):
-            val.append((match.group(1), match.group(2)))
-
-getDependencies()
-
-while val:
-    for item in val:
-        node = tree.get_node(item[0])
-
-        if node is not None:
-            parent = dependencies[item[0]]
-            dependency = dependencies[item[1]]
-            tree.create_node(dependency.referenceId, dependency.referenceId, parent=parent.referenceId, data=dependency)
-            val.remove(item)
-
-tree.show()
-shutil.rmtree(tempDirectoryPath)
+if __name__ == "__main__":
+    main()
